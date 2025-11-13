34771b63c487236810621d87b805b7a41426a71c
한진희 <0200134hjh@gmail.com>
2025-11-14 07:44:12 +0900

    Update evaluator.cpp

--- src/evaluator.cpp
+++ src/evaluator.cpp
@@ -1,11 +1,20 @@
 #include "evaluator.hpp"
+#include "ast.hpp"      // ★ 반드시 필요
+
+#include <memory>
+#include <iostream>
 
 void Evaluator::run(const std::vector<StmtPtr>& stmts) {
     for (auto& s : stmts) {
+
+        // Statement 가 ExprStmt인지 확인
         auto es = std::dynamic_pointer_cast<ExprStmt>(s);
         if (!es) continue;
 
-        if (auto p = std::dynamic_pointer_cast<PrintExpr>(es->expr)) {
+        // ExprStmt 안이 PrintExpr인지 확인
+        auto p = std::dynamic_pointer_cast<PrintExpr>(es->expr);
+        if (p) {
+            // PrintExpr 안이 NumberExpr인지 확인
             auto num = std::dynamic_pointer_cast<NumberExpr>(p->value);
             if (num) {
                 std::cout << "[Swift Runtime] print → " << num->value << "\n";
