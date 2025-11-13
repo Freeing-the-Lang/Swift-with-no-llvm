27325eddbc76a5871f0a316ec94f26107b4ecbda
한진희 <0200134hjh@gmail.com>
2025-11-14 07:23:48 +0900

    Update parser.hpp

--- src/parser.hpp
+++ src/parser.hpp
@@ -1,44 +1,13 @@
 #pragma once
-#include "ast.hpp"
 #include <string>
 #include <vector>
-#include <cctype>
-#include <stdexcept>
+#include "ast.hpp"
 
 class Parser {
 public:
-    Parser(const std::string &src);
-
+    Parser(const std::string& src) : code(src) {}
     std::vector<StmtPtr> parseProgram();
 
 private:
-    // token type
-    enum TokenType {
-        IDENT, NUMBER, STRING, OP,
-        LET, PRINT, TRUE, FALSE,
-        LPAREN, RPAREN, EQUAL, SEMI,
-        END
-    };
-
-    struct Token {
-        TokenType type;
-        std::string text;
-    };
-
-    std::vector<Token> tokens;
-    int pos;
-
-    void tokenize(const std::string &src);
-
-    Token peek() const;
-    Token get();
-    bool match(TokenType t);
-
-    StmtPtr parseStatement();
-    StmtPtr parseLet();
-    StmtPtr parsePrint();
-
-    ExprPtr parseExpr();
-    ExprPtr parseTerm();
-    ExprPtr parseFactor();
+    std::string code;
 };
